60a8b9b480ef5b4ba01a694d3725d0bded11991c
Hardboyyy <hardikkuwar12@gmail.com>
2018-08-29 11:52:32 +0000

    added stock

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-mongoose.connect("mongodb://localhost/Assignment");
+mongoose.connect("mongodb://localhost/hata");
 
 //MODELS
 var productDetails = new mongoose.Schema({
@@ -25,7 +25,8 @@ var orderSchema = new mongoose.Schema({
     companyName: String,
     pincode: Number,
     product: String,
-    quantity: Number
+    quantity: Number,
+    totalprice : Number
 
     //created : {type: Date , default : Date.now}
 });
@@ -40,29 +41,24 @@ var Inventory = mongoose.model("Inventory", inventorySchema);
 
 
 // var event1 = new Product({
-//     name: "mountaindewe",
+//     name: "sprite",
 //     image: "https://cdn.pixabay.com/photo/2016/07/21/11/17/mineral-water-1532300__340.jpg",
 //     Description: "this is cold drink",
 //     price: 34
 // });
-
-// event1.save(function (err, doc) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log("successfully saved: ", doc);
-//     }
+// var event = new Inventory({
+//       productName: "sprite",
+//     quantity: 10
 // });
-// console.log(event1);
+// event.save();
+// event1.save();
 //ROUTES
 app.get("/", function (req, res) {
     Product.find({}, function (err, newProduct) {
         if (err) {
             console.log(err);
         } else {
-            res.render("mainframe", {
-                product: newProduct
-            });
+            res.render("mainframe", {product: newProduct});
         }
     });
 });
@@ -71,35 +67,65 @@ app.get("/product/:id", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("confirm", {
-                product: product
+            Inventory.findOne({productName:product.name},function(err,InventoryStock){
+                if(err){
+                    console.log(err);
+                }else{
+                   res.render("order", {product: product,stock:InventoryStock.quantity}); 
+                }
             });
-            console.log(product);
         }
     });
 
 });
 
 app.post("/confirm", function (req, res) {
-
+        var updating = req.body.order['quantity'];
+//        console.log(req.body.order['quantity']);
+        var productName = req.body.order['product'];
+            //console.log(req.body.order['product']);
     Order.create(req.body.order, function (err, newlyOrder) {
-        if (err) {
+        if (err) 
+        {
             console.log(err);
-        } else {
-            console.log(newlyOrder);
-        }
-    });
-    Order.find({}, function (err, updatedOrder) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("form", {
-                order: updatedOrder
+        } else 
+        {
+            Inventory.updateOne({productName:productName},{$inc:{"quantity": -updating}},function(err,updated){
+                if(err)
+                {
+                    console.log(err);
+                } else
+                    {
+                        console.log(updated);
+                    }
             });
+            res.render("orderSummary", {order: newlyOrder});
+            //console.log(newlyOrder);
         }
     });
+  
 });
 
-app.listen(8000, function () {
+//After Cancelling the order
+app.post("/confirm/:id", function(req,res){
+    console.log(req.params.id);
+    Order.findByIdAndRemove(req.params.id,function(err,founded){
+        if(err){throw err;}
+        else {
+        var updating = founded.quantity;
+         Inventory.updateOne({productName:founded.product},{$inc:{"quantity": updating}} ,function(err,cancelled){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(cancelled);
+        }
+    });
+    res.redirect("/");
+        }
+    });
+  
+});
+
+app.listen(process.env.PORT,process.env.IP, function () {
     console.log("SErver started");
 });
